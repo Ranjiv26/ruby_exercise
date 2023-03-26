5c1fa594b2c617961a63fa281775a8c138ce79d6
Ranjiv26 <128925490+Ranjiv26@users.noreply.github.com>
2023-03-26 23:11:33 +0530

    Add files via upload

---
+++ ex1.rb
@@ -0,0 +1,11 @@
+# Exercise 1
+# A Good First Program
+
+puts "Hello WOrld!" # print Text
+puts "Hello Again" # print Text
+puts "I like ty[ing this."
+puts "This is fun."
+puts "Ya! Printing"
+puts "I'd much rather ypu 'not'." # Print Text with Single Quotes
+puts 'I "said" not touch this.' # Print Text with Double Quotes
+puts "i like not you"
